be152280e43700ff7095663e1e08af1be9d7f3bb
johnstennett24 <93450112+johnstennett24@users.noreply.github.com>
2023-07-25 20:52:54 -0500

    Worked on docs and added best practices to the READMe for future developers.

---
+++ manager/lib/services/data_services.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import '../data/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DataServices {
+  // Instance of FireStore database the app will use.
+  static final _db = FirebaseFirestore.instance;
+
+  // Gets all the stores and Id's
+  static Future<Map<String, dynamic>> getStoreIdQuery(String storeName) async {
+    Map<String, dynamic> map = {};
+    var store = await _db.collection("stores").where("name", isEqualTo: "Steak N Shake").get();
+
+    return map;
+  }
+
+  // Future<Map<String, dynamic>> getSalesByStoreIdQuery() async {
+  //   var docRef = _db.collection("sales").where("")
+  // }
+}
---
+++ manager/lib/views/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:manager/widgets/page_widget.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return const PageWidget();
+  }
+}
