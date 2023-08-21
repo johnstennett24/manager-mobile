a7534337199d575fe149f1929b6a5ad086871257
johnstennett24 <93450112+johnstennett24@users.noreply.github.com>
2023-08-21 16:02:25 -0500

    Cleaning out and getting rid of any mentions of firebase.

--- manager/lib/services/data_services.dart
+++ manager/lib/services/data_services.dart
@@ -1,6 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
-import '../data/firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DataServices {
